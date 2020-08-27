c05893da51e68b885e5c6bb3f30ed8a0ce3fd9f6
SonDA-22011995 <daoansonhvtc@gmail.com>
2020-08-27 18:00:28 +0700

    Create layout in my app

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                
+                
+                <div class="col-md-12">
+                <h4>Bootstrap Snipp for Datatable</h4>
+                <div class="table-responsive">
+        
+                        
+                      <table id="mytable" class="table table-bordred table-striped">
+                           
+                           <thead>
+                           
+                           <th><input type="checkbox" id="checkall" /></th>
+                           <th>First Name</th>
+                            <th>Last Name</th>
+                             <th>Address</th>
+                             <th>Email</th>
+                             <th>Contact</th>
+                              <th>Edit</th>
+                              
+                               <th>Delete</th>
+                           </thead>
+            <tbody>
+            
+            <tr>
+            <td><input type="checkbox" class="checkthis" /></td>
+            <td>Mohsin</td>
+            <td>Irshad</td>
+            <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
+            <td>isometric.mohsin@gmail.com</td>
+            <td>+923335586757</td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+            
+         <tr>
+            <td><input type="checkbox" class="checkthis" /></td>
+            <td>Mohsin</td>
+            <td>Irshad</td>
+            <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
+            <td>isometric.mohsin@gmail.com</td>
+            <td>+923335586757</td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+            
+            
+         <tr>
+            <td><input type="checkbox" class="checkthis" /></td>
+            <td>Mohsin</td>
+            <td>Irshad</td>
+            <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
+            <td>isometric.mohsin@gmail.com</td>
+            <td>+923335586757</td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+            
+            
+            
+         <tr>
+            <td><input type="checkbox" class="checkthis" /></td>
+            <td>Mohsin</td>
+            <td>Irshad</td>
+            <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
+            <td>isometric.mohsin@gmail.com</td>
+            <td>+923335586757</td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+            
+            
+         <tr>
+            <td><input type="checkbox" class="checkthis" /></td>
+            <td>Mohsin</td>
+            <td>Irshad</td>
+            <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
+            <td>isometric.mohsin@gmail.com</td>
+            <td>+923335586757</td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+            <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+            
+           
+            
+           
+            
+            </tbody>
+                
+        </table>
+        
+        
+       
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>            
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
+<!------ Include the above in your HEAD tag ---------->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    </body>
+</html>
